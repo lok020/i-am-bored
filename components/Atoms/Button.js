459cc30369f320c63df5ca80d689c9210f41aa3b
wai-lok <wailokcheng020@gmail.com>
2024-04-12 11:44:52 +0100

    add submit button hover bg

--- components/Atoms/Button.js
+++ components/Atoms/Button.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function Button({type, text}) {
     return (
-        <button className="border-2 border-slate-500 rounded-md px-2" type={type}>{text}</button>
+        <button className="border-2 border-slate-500 rounded-md px-2 hover:bg-sky-100" type={type}>{text}</button>
     );
 }
