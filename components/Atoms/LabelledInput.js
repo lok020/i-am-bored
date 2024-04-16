f84255da6f4355617b7e3769c72849d22b2c32d0
wai-lok <wailokcheng020@gmail.com>
2024-04-16 17:46:31 +0100

    update variable name

--- components/Atoms/LabelledInput.js
+++ components/Atoms/LabelledInput.js
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 
 export default function LabelledInput({type, id, text, placeholder, recommendation=true}) {
     const [input, setInput] = useState('');
-    const [userData, setUserData] = useState({});
+    const [localData, setLocalData] = useState({});
 
     useEffect(() => {
-        localStorage.hasOwnProperty('iambored-users') && setUserData(JSON.parse(localStorage.getItem('iambored-users')));
+        localStorage.hasOwnProperty('iambored-users') && setLocalData(JSON.parse(localStorage.getItem('iambored-users')));
     }, []);
 
     const handleRecommendationClick = (e) => {
@@ -25,8 +25,8 @@ export default function LabelledInput({type, id, text, placeholder, recommendati
                 3. JSON list includes input string (not case sensitive)
                 4. user input not equal to the recommendation
                 */}
-                {recommendation && input && userData[id] &&
-                userData[id].map((data) => data.toLowerCase().includes(input.toLowerCase()) && input.toLowerCase() !== data.toLowerCase() &&
+                {recommendation && input && localData[id] &&
+                localData[id].map((data) => data.toLowerCase().includes(input.toLowerCase()) && input.toLowerCase() !== data.toLowerCase() &&
                     <button key={data} className="flex hover:invert" value={data} onClick={handleRecommendationClick}>
                         <div className={`text-sky-300`}>
                             &#9668;
