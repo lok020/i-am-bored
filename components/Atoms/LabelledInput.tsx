2a2885359d66e08dbd105ac76bdca6b03e8c2f64
wai-lok <wailokcheng020@gmail.com>
2024-07-31 12:37:41 +0100

    fix type error on event click handler with event

--- components/Atoms/LabelledInput.tsx
+++ components/Atoms/LabelledInput.tsx
@@ -13,10 +13,10 @@ const LabelledInput:React.FC<LabelledInputInterface> = ({type, id, text, placeho
     const [input, setInput] = useState('');
     const { localData } = useContext(boredAPIContext);
 
-    console.log(localData);
-
-    const handleRecommendationClick = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInput(e.currentTarget.value);
+    const handleRecommendationClick = (event: React.MouseEvent) => {
+        const input = event.target as HTMLElement;
+        setInput(input.innerText);
+        event.preventDefault();
     }
 
     return (
@@ -33,7 +33,7 @@ const LabelledInput:React.FC<LabelledInputInterface> = ({type, id, text, placeho
                 4. user input not equal to the recommendation
                 */}
                 {recommendation && input && localData[id] &&
-                localData[id].map((data) => data.toLowerCase().includes(input.toLowerCase()) && input.toLowerCase() !== data.toLowerCase() &&
+                localData[id].map((data:string) => data.toLowerCase().includes(input.toLowerCase()) && input.toLowerCase() !== data.toLowerCase() &&
                     <button key={data} className="flex hover:invert" value={data} onClick={handleRecommendationClick}>
                         <div className={`text-sky-300`}>
                             &#9668;
