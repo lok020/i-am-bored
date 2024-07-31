12c50941836099bd52ef9c876fbcd8454663a282
wai-lok <wailokcheng020@gmail.com>
2024-07-31 16:24:50 +0100

    create event handler function

--- components/Atoms/LabelledInput.tsx
+++ components/Atoms/LabelledInput.tsx
@@ -13,17 +13,31 @@ const LabelledInput:React.FC<LabelledInputInterface> = ({type, id, text, placeho
     const [input, setInput] = useState('');
     const { localData } = useContext(boredAPIContext);
 
+    const handleInputUpdate = (e: React.FormEvent<HTMLInputElement>) => {
+        setInput(e.currentTarget.value);
+        e.preventDefault();
+    }
+
     const handleRecommendationClick = (event: React.MouseEvent) => {
         const input = event.target as HTMLElement;
         setInput(input.innerText);
         event.preventDefault();
     }
 
+    const hasRecommendationID = () => {
+        const localData = {};
+        return (
+            recommendation &&
+            input &&
+            localData[id] &&
+            localData[id].map((data:string) => data.toLowerCase().includes(input.toLowerCase()) && input.toLowerCase() !== data.toLowerCase()))
+    }
+
     return (
         <div>
             <label htmlFor={id} className="pr-2">{text}</label>
             <input type={type} id={`${recommendation ? "recommendation-" : ""}${id}`} name={text} value={input} autoComplete="off" required
-            placeholder={placeholder} onChange={(e) => setInput(e.currentTarget.value)}
+            placeholder={placeholder} onChange={handleInputUpdate}
             className="border-2 border-slate-300 rounded-md px-2 py-1"/>
             <div className="inline-flex mt-1">
                 {/* show recommendations when:
