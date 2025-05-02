9cd440ee897ea39956929a9c8b58527b02f33ee9
wai-lok <wailokcheng020@gmail.com>
2025-05-02 14:46:24 +0100

    chore: update font style for all existing pages

--- app/BoredAPI/page.tsx
+++ app/BoredAPI/page.tsx
@@ -15,7 +15,9 @@ export default function BoredAPIPage() {
 
     return (
         <boredAPIContext.Provider value={{fetchData, setFetchData, localData, setLocalData}}>
-            Due to server site(<a href="https://www.boredapi.com/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600" target="_blank">https://www.boredapi.com/</a>) is down, Bored API page is temporary shutdown until further notice. Sorry for the inconvenience.
+            <div className="font-mono text-black/60">
+                Due to server site(<a href="https://www.boredapi.com/" className="text-blue-600 hover:text-blue-800 visited:text-purple-600" target="_blank">https://www.boredapi.com/</a>) is down, Bored API page is temporary shutdown until further notice. Sorry for the inconvenience.
+            </div>
             {/* <BoredAPIForm />
             {fetchData && <BoredAPIResult />} */}
         </boredAPIContext.Provider>
