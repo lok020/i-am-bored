e19a34af2c3f5836a1913b651aee6a41d2a57b9b
wai-lok <wailokcheng020@gmail.com>
2025-05-02 15:02:38 +0100

    chore: move all const into a standalone file, update data structure for list of apps

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,34 @@
 "use client"
 
-export default function Home() {
-  const title = "i-am-bored";
-  const intro = "This is a web application designed to cure your boredom. You may access them through clicking on the header above.";
-  const app1 = "BoredAPI - By answering a specific question, it will suggest you an activities.";
+import { title, intro, activeApp, deprecatedApp } from "@/consts/home";
 
+export default function Home() {
+  
   return (
     <div className="font-mono">
       <h1 className="relative w-[max-content] font-bold text-4xl before:absolute before:inset-0 before:animate-typewriter
       before:bg-neutral-100 after:bg-black after:absolute after:inset-0 after:w-[0.125em] after:animate-caret">{title}</h1>
       <div className="text-black/60">
         <p>{intro}</p>
-        <br/>
-        <ul>
-          <p>{"List of application(s):"}</p>
-          <ol className="ps-3 mt-2 space-y-1 list-decimal list-inside">
-              <li>{app1}</li>
-          </ol>
-        </ul>
+        {activeApp.length > 0 && <>
+          <br/>
+          <ul>
+            <p>{"Active application(s):"}</p>
+            <ol className="ps-3 mt-2 space-y-1 list-decimal list-inside">
+              {activeApp.map(aApp => <li>{aApp}</li>)}
+            </ol>
+          </ul>
+        </>}
+        
+        {deprecatedApp.length > 0 && <>
+          <br/>
+          <ul>
+            <p>{"Deprecated application(s):"}</p>
+            <ol className="ps-3 mt-2 space-y-1 list-decimal list-inside">
+              {deprecatedApp.map(dApp => <li>{dApp}</li>)}
+            </ol>
+          </ul>
+        </>}
       </div>
     </div>
   );
---
+++ consts/home.ts
@@ -0,0 +1,8 @@
+export const title = "i-am-bored";
+export const intro = "This is a web application designed to cure your boredom. You may access them through clicking on the header above.";
+
+export const activeApp = [];
+
+export const deprecatedApp = [
+    "BoredAPI - By answering a specific question, it will suggest you an activities.",
+];
